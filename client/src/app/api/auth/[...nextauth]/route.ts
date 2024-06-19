7895343b656af1030bb3ac4fafbc2f7b1ffa103b
Sebastian Jablonski <sebaa1080@gmail.com>
2024-06-19 20:06:31 +0200

    Seeking help with dockerizing my project

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -20,9 +20,9 @@ function requestRefreshOfAccessToken(token: JWT) {
 export const authOptions: AuthOptions = {
   providers: [
     KeycloakProvider({
-      clientId: `${process.env.KEYCLOAK_CLIENT_ID}`,
-      clientSecret: `${process.env.KEYCLOAK_CLIENT_SECRET}`,
-      issuer: `${process.env.KEYCLOAK_ISSUER}`
+      clientId: process.env.KEYCLOAK_CLIENT_ID!,
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!,
+      issuer: process.env.KEYCLOAK_ISSUER!,
     })
   ],
   pages: {
